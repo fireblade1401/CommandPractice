d8d3b88d490a3c54e22cdbcdc5db7872e8499f47
rusik <rutskoy.ruslan@gmail.com>
2023-09-12 10:41:22 +0600

    ev

--- CommandPractice/settings.py
+++ CommandPractice/settings.py
@@ -75,17 +75,24 @@ WSGI_APPLICATION = 'CommandPractice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog_db',
+#         'USER': 'admin',
+#         'PASSWORD': '111',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog_db',
-        'USER': 'admin',
-        'PASSWORD': '111',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
